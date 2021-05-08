ea514fbd0212592e99f50534425dd09bf043f24d
Peter Roto <peterdroto@gmail.com>
2021-05-08 17:03:53 -0500

    Added navbar and some js for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+/*initialize materialize components*/
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
+
+
+/*CDNJS Variables*/
 var cdnjsBaseUrl = 'https://api.cdnjs.com/libraries'
 var cdnjsSearch = '?search=' + cdnjsUserInput
 var cdnjsUserInput = $('#cdnjsUserInput').val()
