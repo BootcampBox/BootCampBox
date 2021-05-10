2f3a4daa0370eb8b8401a547ddda5a6ab30c2587
Peter Roto <peterdroto@gmail.com>
2021-05-10 13:16:32 -0500

    Got Carousel Buttons working. Did  work on login screen JS,HTML, and CSS . Added H2 tags to define each carousel slide.

---
+++ Scratchpad.md
@@ -0,0 +1,27 @@
+# Scratchpad
+
+## HTML
+
+---
+
+Place in head tag.
+```
+<head>
+<!--Google Sign-in-->
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+   <script src="https://accounts.google.com/gsi/client" async defer></script>
+</head>
+
+
+<body>
+    <div id="g_id_onload" data-client_id="993767314328-rfm0arijtb8gk5h4irlv9cu4hip82lue.apps.googleusercontent.com" data-login_uri="http//localhost:5502" data-ux_mode="redirect" data-auto_prompt="false">
+    </div>
+    <div class="g_id_signin" data-type="standard" data-size="large" data-theme="outline" data-text="sign_in_with" data-shape="rectangular" data-logo_alignment="left">
+    </div>
+</body>
+```
+
+## JS 
+
+---
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,67 @@
 /*initialize materialize components*/
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
+M.AutoInit();
+$(document).ready(function() {
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+});
+/*Carousel Buttons*/
+$('#nxtBtn').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('#carouselFirst').carousel('next');
+});
+$('#prvBtn').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('#carouselFirst').carousel('prev');
 });
 
+
+
+
+/*User Login Form*/
+//Stores CodeSnippets and Bookmarks to localStorage.
+var userList = [{
+        username: "loriculberson",
+        password: "Password1",
+        userSettingsSync: 1,
+    }, {
+        username: "patcorcoran",
+        password: "123456",
+        userSettingsSync: 2,
+
+    }, {
+        username: "test",
+        password: "test",
+        UserSettingssync: 3,
+
+    },
+
+];
+console.log(userList);
+
+
+function check(form) /*function to check userid & password*/ {
+
+    /*the following code checkes whether the entered userid and password are matching*/
+    if (form.useridinput.value == userList.indexof(username) && form.userpassinput.value == userList.indexof(password)) {
+        window.open('index.html') /*opens the target page while Id & password matches*/
+
+        /* Get userSettings from localStorage*/
+
+        /* Load userSettings to correct HTML elements*/
+
+        // initialize();
+    } else {
+        console.log(userList[i].username, userList[i].password)
+        alert("Error Password or Username ") /*displays error message*/
+    }
+}
 
 /*CDNJS Variables*/
 var cdnjsBaseUrl = 'https://api.cdnjs.com/libraries'
 var cdnjsSearch = '?search=' + cdnjsUserInput
-var cdnjsUserInput = $('#cdnjsUserInput').val()
+var cdnjsUserInput = $('#cdnjsUserInput').val();
