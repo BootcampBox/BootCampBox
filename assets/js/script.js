fd208fe93528bb9c1e4127a2f4c942f92ec75521
Peter Roto <peterdroto@gmail.com>
2021-05-11 22:01:57 -0500

    Minor cosmetic tweaks and eliminated uneeded elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 /*initialize materialize components*/
-M.AutoInit();
 $(document).ready(function() {
+    M.AutoInit();
+
     $('.carousel').carousel().height(650);
     $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         indicators: true
     });
 });
-
+$('#snips').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('#carouselFirst').carousel('#four!');
+});
 /*Carousel Buttons*/
 $('#nxtBtn').on('click', function(e) {
     e.preventDefault();
@@ -20,11 +25,26 @@ $('#prvBtn').on('click', function(e) {
     $('#carouselFirst').carousel('prev');
 });
 
+// // Google Sign-in
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// } { /* <a href="#" onclick="signOut();">Sign out</a> */ }
+
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function() {
+//         console.log('User signed out.');
+//     });
+// }
 
 
 
 /*User Login Form*/
-//Stores CodeSnippets and Bookmarks to localStorage.
+// Stores CodeSnippets and Bookmarks to localStorage.
 var userList = [{
         username: "loriculberson",
         password: "Password1",
