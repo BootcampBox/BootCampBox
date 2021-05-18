f367e5934a8995c7dc45033476b21956c9da616e
Rudie G <rauoldm@gmail.com>
2021-05-18 13:04:48 -0500

    Links functionality fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,8 +168,6 @@ $('#cdnjsBtn').on('click', function() {
         });
     })
     // Create variables to make <li> and variables to refer to the input fields
-var linksListEl = $('<li>');
-var linksAnchor = $("<a class='scroll linksLi'>")
 var linkInput = $('#linkInput');
 var nameInput = $('#nameInput');
 var linksBtn = $("#linksBtn");
@@ -181,17 +179,34 @@ function linkMaker() {
     // for localStorage, use numbers for the name of the variables so you can iterate thru with a for loop and backtick syntax
     var linksInputAnchor = $("<a class='scroll linksLi'>");
     var linksListInputEl = $('<li>');
-    localStorage.setItem("links", linkInput.val());
     linksInputAnchor.text(linkInput.val());
     // Append <li> to <ul>
     linksList.append(linksListInputEl);
     linksListInputEl.append(linksInputAnchor);
+    storedLinks.links.push(linkInput.val());
+    localStorage.setItem("stored-links", JSON.stringify(storedLinks));
+    console.log(storedLinks);
     linkInput.val("");
+    localStorage.setItem("stored-links", JSON.stringify(storedLinks));
 }
 // On page load, add the values from localStorage to <li>s and add them (hidden) to the <ul>
-var storedLinks = localStorage.getItem("links");
-console.log(storedLinks);
-var storedLinksAnchor = $("<a class='scroll linksLi'>")
-linksList.append(linksListEl);
-storedLinksAnchor.text(storedLinks);
-linksListEl.append(storedLinksAnchor);
+if (!localStorage.getItem("stored-links") == null || localStorage.getItem("stored-links") == undefined) {
+    localStorage.setItem("stored-links", JSON.stringify({
+        links: [
+        "https://github.com/public-apis/public-apis",
+        "https://unicode.org/emoji/charts/full-emoji-list.html",
+        "https://www.w3schools.com/", 
+        "https://developer.mozilla.org/en-US/", 
+        "https://devhints.io/", 
+        "https://jquery.com/download/"
+    ]}));
+}
+console.log(localStorage.getItem("stored-links"));
+var storedLinks = JSON.parse(localStorage.getItem("stored-links"));
+for (i= 0; i < storedLinks.links.length; i++) {
+    var linksListEl = $('<li>');
+    var linksAnchor = $("<a class='scroll linksLi'>");
+    linksAnchor.text(storedLinks.links[i]);
+    linksList.append(linksListEl);
+    linksListEl.append(linksAnchor);
+}
