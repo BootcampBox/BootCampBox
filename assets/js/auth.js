bb6c267fecbd7ccd8748c6c92ae3dd640ce6799d
Peter Roto <peterdroto@gmail.com>
2021-05-20 00:27:39 -0500

    moved links iterator to auth js so links are populated on log-in, this allows for the firestore saved links to be loaded and appended correctly. Changed localStorage method of snips to match links style. coded save.js to handle "Savebtn" . . .mostly

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,75 +1,74 @@
 //Listen for login state changes
 auth.onAuthStateChanged(function(user) {
-    //Get Data
-    if (user) {
-        fireStore.collection('fsSnips').get().then(function(snapshot) {
-            setupSnips(snapshot.docs);
-            setupUI(user);
-            console.log('user logged in: ', user.email);
-        }, function(err) {
-            console.log(err.message)
-        });
-
-    } else {
-        setupUI()
-        setupSnips([])
-        console.log('user logged out');
-    }
+  //Get Data
+  if (user) {
+    restoreLinks();
+    setupData(user);
+    setupUI(user);
+    console.log('user logged in: ', user.email);
+    console.log('uid is: ', user.uid);
+  } else {
+    setupUI()
+    // setupSnips([])
+    console.log('user logged out');
+  }
 });
 
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', function(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-    const username = signupForm['signup-user'].value;
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+  const username = signupForm['signup-user'].value;
 
-    // sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        return fireStore.collection('users').doc(cred.user.uid).set({ username: username });
-
-    }).then(function() { // close the signup modal & reset form
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
-    })
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return cred.user.updateProfile({
+      displayName: username
+    });
+  }).then(function() { // close the signup modal & reset form
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  })
 
 });
 
 // logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', function(e) {
-    e.preventDefault();
-    auth.signOut();
+  e.preventDefault();
+  $('#linksList').children().remove();
+  auth.signOut();
 })
 
 // login
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', function(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // get user info
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
 
-    // log the user in
-    auth.signInWithEmailAndPassword(email, password).then((cred => {
-        console.log(cred.user)
-            // close the signup modal & reset form
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-    }), function(error) {
-        var errMessage = error.message;
-        $('#modal-error').modal('open');
-        $('#modal-login').modal('close');
-        $('#errmsg').text(errMessage);
-        $('#posSolution').html('Try using the <a href="#signup-form">Sign Up</a> option.');
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred => {
+    console.log(cred.user)
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  }), function(error) {
+    var errMessage = error.message;
+    $('#modal-error').modal('open');
+    $('#modal-login').modal('close');
+    $('#errmsg').text(errMessage);
+    $('#posSolution').html('Try using the <a href="#signup-form">Sign Up</a> option.');
 
-        console.log(errMessage)
-    })
+    console.log(errMessage)
+  })
 
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Setup Materialize Components
 document.addEventListener('DOMContentLoaded', function() {
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
 });
 
 /*************************************************************************\
@@ -12,48 +12,62 @@ document.addEventListener('DOMContentLoaded', function() {
 const accountDetails = $('.account-details');
 
 const setupUI = function(user) {
-    console.log('setupUI Fired');
-    if (user) {
-        //toggle UI elements
-        $('.logged-in').show();
-        $('.logged-out').hide();
-        $('#greeting').show();
-        $('#emailDisplay').text(user.email);
-        //account info 
-        fireStore.collection('users').doc(user.uid).get().then(function(doc) {
-            const userInfo = `
+  console.log('setupUI Fired');
+  if (user) {
+    console.log(user.uid);
+    //toggle UI elements
+    $('.logged-in').show();
+    $('.logged-out').hide();
+    $('#greeting').show();
+    $('#emailDisplay').text(user.email);
+    //account info
+    fireStore.collection('users').doc(user.uid).get().then(function(doc) {
+        const userInfo = `
         <div id="acctInfoEl">
-        <p><span>Username:</span>${doc.data().username}</p>
+        <p><span>Username:</span>${user.displayName}</p>
         <p><span>Email:</span>${user.email}</p>
         </div>`;
-            accountDetails.append(userInfo);
-        });
-    } else {
-        //hide account info
-        accountDetails.innerHTML = '';
-        $('.logged-in').hide();
-        $('.logged-out').show();
-        $('#greeting').hide();
-    };
+        accountDetails.append(userInfo);
+        // setupLinks(user)
+      },
+      function(err) {
+        console.log(err);
+      })
+  } else {
+    //hide account info
+    accountDetails.innerHTML = '';
+    $('.logged-in').hide();
+    $('.logged-out').show();
+    $('#greeting').hide();
+  };
 
+}
+
+const setupData = function(user) {
+  fireStore.collection('users').doc(user.uid).get().then(function(doc) {
+    if (doc.exists) {
+      console.log(doc.data())
+      localStorage.setItem('Local Snippets Titles', doc.data().snippets[0])
+      console.log(doc.data().snippets[0])
+      localStorage.setItem('Local Snippets Inputs', doc.data().snippets[1])
+      console.log(doc.data().snippets[1])
+      localStorage.setItem('stored-links', doc.data().links)
+      console.log(doc.data().links)
+    } else {
+      console.log('No such Document');
+    }
+    // localStorage.setItem('stored-links', user.uid.links);
+    // localStorage.setItem('Local Snippets Titles', );
+    // localStorage.setItem('Local Snippets Inputs', uid.snippets[1]);
+  })
 }
 
 //Calendar Events(?)
+var now = dayjs()
+console.log(dayjs(now).format('dddd'))
 
 //Links
 
-
-//Code Snippets
-const setupSnips = function(data) {
-    data.forEach(function(doc) {
-        const codesnip = doc.data();
-        // Create template for how to handle data as it returns
-        const li = `<pre class="snippet"
-        </li>`;
-        html += li;
-        console.log(codesnip)
-    })
-}
 
 // //Set Snippets to firestore
 // const snipRef = fireStore.collection('codesnippets')
@@ -70,38 +84,41 @@ const setupSnips = function(data) {
 
 //Nav navTabs
 $('#navDash').on('click', function() {
-    $('#workspace').children().hide();
-    $('#dash').show();
+  $('#workspace').children().hide();
+  $('#dash').show();
 
 })
+
 $('#navLinks').on('click', function() {
-    $('#workspace').children().hide();
-    $('#links').show();
+  $('#workspace').children().hide();
+  $('#links').show();
 
 })
+
 $('#navSlack').on('click', function() {
-    $('#workspace').children().hide();
-    $('#links').show();
+  $('#workspace').children().hide();
+  $('#links').show();
 
 })
+
 $('#navNotion').on('click', function() {
-    $('#workspace').children().hide();
-    $('#Notion').show();
+  $('#workspace').children().hide();
+  $('#Notion').show();
 
 })
 
 $('#navCdnjs').on('click', function() {
-    $('#workspace').children().hide();
-    $('#cdnjs').show();
+  $('#workspace').children().hide();
+  $('#cdnjs').show();
 })
 
 $('#navLinks').on('click', function() {
-    $('#workspace').children().hide();
-    $('#links').show();
+  $('#workspace').children().hide();
+  $('#links').show();
 })
 
-//close modal if popped 
+//close modal if popped
 $('#modal-cdnjs--close').on('click', function() {
-    $('#cdnjsInput').val('');
-    $('#modal-cdnjs').modal('close');
+  $('#cdnjsInput').val('');
+  $('#modal-cdnjs').modal('close');
 })
