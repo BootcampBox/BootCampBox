c15bdceec312e0b6f07e8e117b99f0a3f7c90a99
Peter Roto <peterdroto@gmail.com>
2021-05-20 01:11:54 -0500

    fixed some immediate issues with last commit, Links not coming back if null, etc.

--- assets/js/save.js
+++ assets/js/save.js
@@ -1,21 +1,20 @@
 // Saving data
 
 $('#saveBtn').on('click', function(user) {
-  var user = firebase.auth().currentUser;
-  console.log(user);
-  var snippets, links;
-  var snipT_LS = localStorage.getItem('Local Snippets Titles');
-  var snipI_LS = localStorage.getItem('Local Snippets Inputs');
-  var links_LS = localStorage.getItem('stored-links');
-  // User is signed in. uid is now available to use
-  fireStore.collection('users')
-    .doc(user.uid)
-    .update({
-      snippets: [snipT_LS, snipI_LS, ],
+    var user = firebase.auth().currentUser;
+    console.log(user);
+    var snips_LS = localStorage.getItem('stored-snips');
+    // var snipI_LS = localStorage.getItem('Local Snippets Inputs');
+    var links_LS = localStorage.getItem('stored-links');
+    // User is signed in. uid is now available to use
+    fireStore.collection('users')
+        .doc(user.uid)
+        .update({
+            snippets: snips_LS,
 
-      links: links_LS
-    }, {
-      merge: true
-    });
-  console.log(fireStore.collection('users').doc(user.uid));
+            links: links_LS
+        }, {
+            merge: true
+        });
+    console.log(fireStore.collection('users').doc(user.uid));
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
+($(function() {
+    var linksCheck = localStorage.getItem('stored-links');
+    if (linksCheck == null) {
+        console.log('LocalStorage was Empty')
+        localStorage.setItem('stored-links', JSON.stringify({
+            links: [
+                "https://github.com/public-apis/public-apis",
+                "https://unicode.org/emoji/charts/full-emoji-list.html",
+                "https://www.w3schools.com/",
+            ]
+        }))
+    }
+}))
 // Setup Materialize Components
 document.addEventListener('DOMContentLoaded', function() {
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
 });
 
 /*************************************************************************\
@@ -12,54 +25,64 @@ document.addEventListener('DOMContentLoaded', function() {
 const accountDetails = $('.account-details');
 
 const setupUI = function(user) {
-  console.log('setupUI Fired');
-  if (user) {
-    console.log(user.uid);
-    //toggle UI elements
-    $('.logged-in').show();
-    $('.logged-out').hide();
-    $('#greeting').show();
-    $('#emailDisplay').text(user.email);
-    //account info
-    fireStore.collection('users').doc(user.uid).get().then(function(doc) {
-        const userInfo = `
+    console.log('setupUI Fired');
+    if (user) {
+        var fsLinks = JSON.parse(localStorage.getItem("fs-links"));
+        for (i = 0; i < fsLinks.links.length; i++) {
+            var linksListEl = $('<li>');
+            var linksAnchor = $("<a class='scroll linksLi'>");
+            linksAnchor.text(fsLinks.links[i]);
+            linksList.append(linksListEl);
+            linksListEl.append(linksAnchor);
+        };
+        console.log(user.uid);
+
+        restoreLinks();
+        //toggle UI elements
+        $('.logged-in').show();
+        $('.logged-out').hide();
+        $('#greeting').show();
+        $('#emailDisplay').text(user.email);
+        //account info
+        fireStore.collection('users').doc(user.uid).get().then(function(doc) {
+                const userInfo = `
         <div id="acctInfoEl">
         <p><span>Username:</span>${user.displayName}</p>
         <p><span>Email:</span>${user.email}</p>
         </div>`;
-        accountDetails.append(userInfo);
-        // setupLinks(user)
-      },
-      function(err) {
-        console.log(err);
-      })
-  } else {
-    //hide account info
-    accountDetails.innerHTML = '';
-    $('.logged-in').hide();
-    $('.logged-out').show();
-    $('#greeting').hide();
-  };
+                accountDetails.append(userInfo);
+                // setupLinks(user)
+            },
+            function(err) {
+                console.log(err);
+            })
+    } else {
+        //hide account info
+        accountDetails.innerHTML = '';
+        $('.logged-in').hide();
+        $('.logged-out').show();
+        $('#greeting').hide();
+    };
 
 }
 
 const setupData = function(user) {
-  fireStore.collection('users').doc(user.uid).get().then(function(doc) {
-    if (doc.exists) {
-      console.log(doc.data())
-      localStorage.setItem('Local Snippets Titles', doc.data().snippets[0])
-      console.log(doc.data().snippets[0])
-      localStorage.setItem('Local Snippets Inputs', doc.data().snippets[1])
-      console.log(doc.data().snippets[1])
-      localStorage.setItem('stored-links', doc.data().links)
-      console.log(doc.data().links)
-    } else {
-      console.log('No such Document');
-    }
-    // localStorage.setItem('stored-links', user.uid.links);
-    // localStorage.setItem('Local Snippets Titles', );
-    // localStorage.setItem('Local Snippets Inputs', uid.snippets[1]);
-  })
+    fireStore.collection('users').doc(user.uid).get().then(function(doc) {
+        if (doc.exists) {
+            console.log(doc.data())
+            localStorage.setItem('fs-snippets', doc.data().snippets)
+                // console.log(doc.data().snippets[0])
+                // localStorage.setItem('Local Snippets Inputs', doc.data().snippets[1])
+                // console.log(doc.data().snippets[1])
+            localStorage.setItem('fs-links', doc.data().links)
+            console.log(doc.data().links)
+        } else {
+            console.log('No such Document');
+        }
+        // localStorage.setItem('stored-links', user.uid.links);
+        // localStorage.setItem('Local Snippets Titles', );
+        // localStorage.setItem('Local Snippets Inputs', uid.snippets[1]);
+    })
 }
 
 //Calendar Events(?)
@@ -84,41 +107,41 @@ console.log(dayjs(now).format('dddd'))
 
 //Nav navTabs
 $('#navDash').on('click', function() {
-  $('#workspace').children().hide();
-  $('#dash').show();
+    $('#workspace').children().hide();
+    $('#dash').show();
 
 })
 
 $('#navLinks').on('click', function() {
-  $('#workspace').children().hide();
-  $('#links').show();
+    $('#workspace').children().hide();
+    $('#links').show();
 
 })
 
 $('#navSlack').on('click', function() {
-  $('#workspace').children().hide();
-  $('#links').show();
+    $('#workspace').children().hide();
+    $('#links').show();
 
 })
 
 $('#navNotion').on('click', function() {
-  $('#workspace').children().hide();
-  $('#Notion').show();
+    $('#workspace').children().hide();
+    $('#Notion').show();
 
 })
 
 $('#navCdnjs').on('click', function() {
-  $('#workspace').children().hide();
-  $('#cdnjs').show();
+    $('#workspace').children().hide();
+    $('#cdnjs').show();
 })
 
 $('#navLinks').on('click', function() {
-  $('#workspace').children().hide();
-  $('#links').show();
+    $('#workspace').children().hide();
+    $('#links').show();
 })
 
 //close modal if popped
 $('#modal-cdnjs--close').on('click', function() {
-  $('#cdnjsInput').val('');
-  $('#modal-cdnjs').modal('close');
+    $('#cdnjsInput').val('');
+    $('#modal-cdnjs').modal('close');
 })
