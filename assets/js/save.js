7eed384662fa31efada255a120290546265e49c9
Peter Roto <peterdroto@gmail.com>
2021-05-20 01:52:01 -0500

    started adding necessary variables for calendar FS sync. . . . not done.

--- assets/js/save.js
+++ assets/js/save.js
@@ -3,9 +3,23 @@
 $('#saveBtn').on('click', function(user) {
     var user = firebase.auth().currentUser;
     console.log(user);
+
+
+
+    var mon = localStorage.getItem('Monday')
+    var tue = localStorage.getItem('Tuesday')
+    var wed = localStorage.getItem('Wednesday')
+    var thu = localStorage.getItem('Thursday')
+    var fri = localStorage.getItem('Friday')
+    var sat = localStorage.getItem('Saturday')
+    var sun = localStorage.getItem('Sunday')
+    var calendar_LS = [{ mon }, { tue }, { wed }, { thu }, { fri }, { sat }, { sun }]
+    console.log(calendar_LS)
+
     var snips_LS = localStorage.getItem('stored-snips');
     // var snipI_LS = localStorage.getItem('Local Snippets Inputs');
     var links_LS = localStorage.getItem('stored-links');
+
     // User is signed in. uid is now available to use
     fireStore.collection('users')
         .doc(user.uid)
