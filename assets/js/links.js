afa017f682a498d0a92d9672037bb3a17d0c2896
Peter Roto <peterdroto@gmail.com>
2021-05-26 01:17:06 -0500

    Snippets is now 100% working for additions (didn't check remove), persistent through log out and localStorage wipe, adds re-appends on loggin!!!

--- assets/js/links.js
+++ assets/js/links.js
@@ -5,70 +5,57 @@ var linksBtn = $("#linksBtn");
 var linksList = $("#linksList");
 var linksRemoverA = $("#linksRemoverA");
 var linksRemoverB = $("#linksRemoverB");
+
 // Add an event listener to the add button, which takes value from link and name input fields, save to localStorage, apply to <li> variable
 linksBtn.on("click", linkMaker)
 linksRemoverA.on("click", theLinksRemover);
 linksRemoverB.on("click", theLinksRemover);
 
-// var lsLinks = JSON.parse(localStorage.getItem("stored-links"));
 
-function appendlsLinks(lsLinks) {
-    console.log('appendlslinks has fired')
-    var lsLinks = JSON.parse(localStorage.getItem("stored-links"))
-    console.log(lsLinks);
-    for (i = 0; i < lsLinks.links.length; i++) {
-        var linksListEl = $('<li>');
-        var linksAnchor = $("<a class='scroll linksLi'>");
-        linksAnchor.text(lsLinks.links[i]);
-        linksList.append(linksListEl);
-        linksListEl.append(linksAnchor);
-    }
-}
-
-function appendfsLinks(fsLinks) {
-    console.log('appendfslinks has fired');
-    var fsLinks = JSON.parse(localStorage.getItem("fs-links"));
-    for (var i = 0; i < fsLinks.links.length; i++) {
-        var linksListEl = $('<li>');
-        var linksAnchor = $("<a class='scroll linksLi'>");
-        linksAnchor.text(fsLinks.links[i]);
-        console.log(fsLinks.links[i]);
-        linksList.append(linksListEl);
-        linksListEl.append(linksAnchor);
-    }
+function appendLinks(setUpLinks) {
+  console.log('appendlinks has fired')
+  var lsLinks = localStorage.getItem("links")
+  console.log(setUpLinks);
+  for (i = 0; i < setUpLinks.length; i++) {
+    var linksListEl = $('<li>');
+    var linksAnchor = $("<a class='scroll linksLi'>");
+    linksAnchor.text(lsLinks.links[i]);
+    linksList.append(linksListEl);
+    linksListEl.append(linksAnchor);
+  }
 }
 
 function linkMaker() {
-    var linksInputAnchor = $("<a class='scroll linksLi'>");
-    var linksListInputEl = $('<li>');
-    linksInputAnchor.text(linkInput.val());
-    linksList.append(linksListInputEl);
-    linksListInputEl.append(linksInputAnchor);
-    console.log(lsLinks.links);
-    lsLinks.links.push(linkInput.val());
-    localStorage.setItem("stored-links", JSON.stringify(lsLinks));
-    console.log(lsLinks);
-    linkInput.val("");
-    localStorage.setItem("stored-links", JSON.stringify(lsLinks));
+  var linksInputAnchor = $("<a class='scroll linksLi'>");
+  var linksListInputEl = $('<li>');
+  linksInputAnchor.text(linkInput.val());
+  linksList.append(linksListInputEl);
+  linksListInputEl.append(linksInputAnchor);
+  console.log(lsLinks.links);
+  lsLinks.links.push(linkInput.val());
+  localStorage.setItem("links", JSON.stringify(lsLinks));
+  console.log(lsLinks);
+  linkInput.val("");
+  localStorage.setItem("links", JSON.stringify(lsLinks));
 }
 
 // TODO: localStorage removals
 function theLinksRemover() {
   var theLinks = $(".linksLi");
-  var lsLinks = JSON.parse(localStorage.getItem("stored-links"));
+  var lsLinks = JSON.parse(localStorage.getItem("links"));
   lsLinksLinks = lsLinks.links;
-  if ($(this).attr("class") == "fullRemover") {   
-  // Remove Loop
-  for (var i = 0; i < theLinks.length; i++) {
-  theLinks[i].remove();
-  lsLinks.pop();
-  localStorage.setItem("stored-links", JSON.stringify(lsLinks));
-  }
-  } else if ($(this).attr("class") == "lastRemover") {
-      var i = theLinks.length - 1;
+  if ($(this).attr("class") == "fullRemover") {
+    // Remove Loop
+    for (var i = 0; i < theLinks.length; i++) {
       theLinks[i].remove();
-      lsLinksLinks.pop();
-      localStorage.setItem("stored-links", JSON.stringify(lsLinks));
+      lsLinks.pop();
+      localStorage.setItem("links", JSON.stringify(lsLinks));
+    }
+  } else if ($(this).attr("class") == "lastRemover") {
+    var i = theLinks.length - 1;
+    theLinks[i].remove();
+    lsLinksLinks.pop();
+    localStorage.setItem("links", JSON.stringify(lsLinks));
   }
 }
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,109 @@
 // Setup Materialize Components
 document.addEventListener('DOMContentLoaded', function() {
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
 });
+//Declare Storage Items
+const snippetsGet = localStorage.getItem("snippets");
+const linksGet = localStorage.getItem("links");
 
 function placeholderItems() {
-    var linksCheck = localStorage.getItem('stored-links');
-    if (!linksCheck) {
-        console.log('LocalStorage was Empty')
-        localStorage.setItem('stored-links', JSON.stringify({
-            links: [
-                "https://github.com/public-apis/public-apis",
-                "https://unicode.org/emoji/charts/full-emoji-list.html",
-                "https://www.w3schools.com/",
-            ]
-        }))
-    }
-    if (localStorage.getItem('ls-snippets') == null) {
-        localStorage.setItem('ls-snippets', JSON.stringify({
-            snippets: [
-                'This is a snippet Title saved in localStorage:',
-                'This is a snippet. Saved to localStorage whenever you click save'
-            ]
-        }));
-    }
-    if (localStorage.getItem('fs-snippets') == null || localStorage.getItem('fs-snippets') == '') {
-        localStorage.setItem('fs-snippets', JSON.stringify({
-            snippets: [
-                'This is a snippet Title saved with FireStore:',
-                'This is a snippet, now you try! Click Save.'
-            ]
-        }));
-    }
-};
+  if (!linksGet || linksGet == '') {
+    console.log('LocalStorage was Empty')
+    localStorage.setItem('links', JSON.stringify(
+      [
+        "https://github.com/public-apis/public-apis",
+        "https://unicode.org/emoji/charts/full-emoji-list.html",
+        "https://www.w3schools.com/"
+      ]
+    ))
+  }
+  if (localStorage.getItem('snippets') == null || localStorage.getItem('snippets') == '') {
+    localStorage.setItem('snippets', {
+      snippets: [
+        'This is a snippet Title saved in localStorage:',
+        'This is a snippet. Saved to localStorage whenever you click save'
+      ]
+    })
 
+  };
+}
 /*************************************************************************\
  **** Appending the DOM with info from Firestore on login******************\
  *** IF YOU SEE SOMETHING I'M FORGETTING PLEASE ADD THE PSEUDOCODE FOR IT**\
  **************************************************************************/
-// const userDBget
-const accountDetails = $('.account-details');
+
+
+
+
 
 //Toggle UI based on signed in state
 function setupUI(user) {
-    console.log('setupUI Fired');
-    if (user) {
-        //toggle UI elements
-        $('.logged-in').show();
-        $('.logged-out').hide();
-        $('#greeting').show();
-        $('#emailDisplay').text(user.email);
-        //account info
-        fireStore.collection('users').doc(user.uid).get().then(function(doc) {
-            const userInfo = `
+  console.log('setupUI Fired');
+  if (user) {
+    //toggle UI elements
+    $('.logged-in').show();
+    $('.logged-out').hide();
+    $('#greeting').show();
+    $('#emailDisplay').text(user.email);
+    //account info
+    fireStore.collection('users').doc(user.uid).get().then(function(doc) {
+      const userInfo = `
           <div id="acctInfoEl">
           <p><span>Username:</span>${user.displayName}</p>
           <p><span>Email:</span>${user.email}</p>
           </div>`;
-            accountDetails.append(userInfo);
+      accountDetails.append(userInfo);
+    });
+  }
+  setupData(user);
+}
 
-            // setupLinks(user)
-        });
-    }
-    setupData(user);
+var snipObj = {
+  snippets: []
 }
 
 function setupData(user) {
-    console.log('setupData has fired');
-    //Go retireve the user data from Firestore
-    fireStore.collection('users').doc(user.uid).get().then(function(doc) {
-        //If there is user data proceeed
-        if (doc.exists) {
-            console.log(doc.data())
-            localStorage.setItem('fs-snippets', doc.data().snippets)
-            localStorage.setItem('fs-links', doc.data().links)
-            console.log(doc.data().links)
-                //If not, don't.
-        } else {
-            console.log('No such Document');
-        }
-        /*After the above function has fired, invoke next function.
-        This prevents items from coming back as null or undefined
-        if it takes longer than expected to get the data back.*/
-    }).then(function() {
-        restoreData(user)
-    });
-}
+  console.log('setupData has fired');
+  console.log(user.uid);
+  let userDocRef = fireStore.collection('users').doc(user.uid)
+  //Go retireve the user data from Firestore
+  userDocRef.get('snippets').then((doc) => {
+    console.log(doc.data().snippets)
+    //If there is user data proceeed
+    if (doc.exists && doc.data() != null) {
+      for (var i = 0; i < doc.data().snippets.snippets.length; i++) {
+        snipObj.snippets.push(doc.data().snippets.snippets[i])
+      }
+      console.log(snipObj)
+      let setUpSnips = JSON.stringify(snipObj)
+      console.log('setUpSnips=', setUpSnips)
+      localStorage.setItem('snippets', setUpSnips)
 
-function appendlsLinks(lsLinks) {
-    console.log('appendlslinks has fired')
-    var lsLinks = JSON.parse(localStorage.getItem("stored-links"))
-    console.log(lsLinks);
-    for (i = 0; i < lsLinks.links.length; i++) {
-        var linksListEl = $('<li>');
-        var linksAnchor = $("<a class='scroll linksLi'>");
-        linksAnchor.text(lsLinks.links[i]);
-        linksList.append(linksListEl);
-        linksListEl.append(linksAnchor);
+      // var setUpLinks = {
+      //   links: JSON.stringify([doc.data().links])
+      // }
+      //
+      // localStorage.setItem('links', JSON.parse(setUpLinks))
+
+      appendSnips();
+      // appendLinks(setUpLinks);
+      //If not, don't.
+    } else {
+      console.log('No such Document');
     }
+    /*After the above function has fired, invoke next function.
+    This prevents items from coming back as null or undefined
+    if it takes longer than expected to get the data back.*/
+  })
 }
+// .then(function() {
+//   restoreData()
+// });
+
 
 //Runs through the different functions and appends correlating items to the page.
-function restoreData() {
-    console.log('restoreData has fired')
-        //Create local variables for each local storage key
-    var lsSnips = JSON.parse(localStorage.getItem("ls-snippets"));
-    // var fsSnips = JSON.parse(localStorage.getItem("fs-snippets"));
-    var lsLinks = JSON.parse(localStorage.getItem("stored-links"))
-    console.log(lsLinks);
-    // var fsLinks = JSON.parse(localStorage.getItem("fs-links"));
-    // console.log('fsLinks=', fsLinks);
-    console.log('lsLinks=', lsLinks);
-    // console.log('fsSnips=', fsSnips);
-    console.log('lsSnips=', lsSnips);
-    //If the item is not empty, append the contents to the page
-    // if (fsLinks.links.length >= 0) {
-    //     appendfsLinks(fsLinks);
-    // }
-    //If the item is not empty, append the contents to the page
-    // if (fsLinks.links.length == undefined) {
-    //     window.reload();
-    //     setupData();
-    // }
-    //If the item is not empty, append the contents to the page
-    if (lsLinks) {
-        appendlsLinks(lsLinks);
-    }
-    //If the item is not empty, append the contents to the page
-    // if (!fsSnips) {
-    placeholderItems();
-    // } else if (fsSnips.snippets.length >= 0 && fsSnips.snippets != null) {
-    // appendfsSnips(fsSnips);
-    // }
-    //If the item is not empty, append the contents to the page
-    if (lsSnips) {
-        appendlsSnips(lsSnips);
-    }
-}
+// function restoreData() {}
 
 
 
@@ -152,71 +119,71 @@ console.log(dayjs(now).format('dddd'))
 //LinksClock
 
 var renderClock = function() {
-    $("#currentTime").text(dayjs().format("h:mm a"));
-    var now = dayjs().format("dddd, MMM / D / YYYY");
-    $("#currentDay").text(now)
+  $("#currentTime").text(dayjs().format("h:mm a"));
+  var now = dayjs().format("dddd, MMM / D / YYYY");
+  $("#currentDay").text(now)
 }
 
 //Render clock every second
 $(function() {
-    setInterval(renderClock, 1000);
+  setInterval(renderClock, 1000);
 });
 //Nav navTabs
 $('#navDash').on('click', function() {
-    $('#workspace').children().hide();
-    $('#dash').show();
+  $('#workspace').children().hide();
+  $('#dash').show();
 
 })
 
 $('#navGit').on('click', function() {
-    $('#workspace').children().hide();
-    $('#git').show();
+  $('#workspace').children().hide();
+  $('#git').show();
 
 })
 
 $('#navCdnjs').on('click', function() {
-    $('#workspace').children().hide();
-    $('#cdnjs').show();
+  $('#workspace').children().hide();
+  $('#cdnjs').show();
 })
 
 $('#navCal').on('click', function() {
-    $('#workspace').children().hide();
-    $('#calendar').show();
+  $('#workspace').children().hide();
+  $('#calendar').show();
 })
 
 //close modal if popped
 $('#modal-cdnjs--close').on('click', function() {
-    $('#cdnjsInput').val('');
-    $('#modal-cdnjs').modal('close');
+  $('#cdnjsInput').val('');
+  $('#modal-cdnjs').modal('close');
 });
 
 // Week Planner
 
 function getEventText() {
-    var eventText = $(this).next(".inputField").val();
-    console.log($(this).next(".inputField").attr("id"))
-    var eventID = $(this).next(".inputField").attr("id");
-    localStorage.setItem(eventID, eventText);
+  var eventText = $(this).next(".inputField").val();
+  console.log($(this).next(".inputField").attr("id"))
+  var eventID = $(this).next(".inputField").attr("id");
+  localStorage.setItem(eventID, eventText);
 }
 $('.saveBtn').on('click', getEventText);
 $(function() {
-    var dayList = $(".inputField");
-    // console.log(dayList.length);
-    for (var i = 0; i < dayList.length; i++) {
-        // console.log(dayList[i]);
-        var dayStorage = dayList[i].id;
-        var setEventText = localStorage.getItem(dayStorage);
-        // console.log(setEventText);
-        $(`#${dayStorage}`).val(setEventText);
-    };
+  var dayList = $(".inputField");
+  // console.log(dayList.length);
+  for (var i = 0; i < dayList.length; i++) {
+    // console.log(dayList[i]);
+    var dayStorage = dayList[i].id;
+    var setEventText = localStorage.getItem(dayStorage);
+    // console.log(setEventText);
+    $(`#${dayStorage}`).val(setEventText);
+  };
 })
 $("#clearBtn").on("click", function() {
-    var dayList = $(".inputField");
-    console.log(dayList);
-    for (var i = 0; i < dayList.length; i++) {
-        console.log(dayList[i]);
-        console.log(`${dayList[i].id}`);
-        $(`#${dayList[i].id}`).val("");
-        localStorage.removeItem(`${dayList[i].id}`)
-    }
+  var dayList = $(".inputField");
+  console.log(dayList);
+  for (var i = 0; i < dayList.length; i++) {
+    console.log(dayList[i]);
+    console.log(`${dayList[i].id}`);
+    $(`#${dayList[i].id}`).val("");
+    localStorage.removeItem(`${dayList[i].id}`)
+  }
 });
