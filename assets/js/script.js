053df6232148efc83f61d7b82dbbaf85d3a4fd0b
Peter Roto <peterdroto@gmail.com>
2021-05-26 19:53:33 -0500

    Got the SNippets working for FireStore both additions and removes!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,42 @@
 // Setup Materialize Components
 document.addEventListener('DOMContentLoaded', function() {
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
 });
+
+//global remove from array function
+Array.prototype.remove = function(value) {
+    const index = this.findIndex(obj => obj[key] === value);
+    return index >= 0 ? [
+        ...this.slice(0, index),
+        ...this.slice(index + 1)
+    ] : this;
+};
+
 //Declare Storage Items
 const snippetsGet = localStorage.getItem("snippets");
 const linksGet = localStorage.getItem("links");
 
 function placeholderItems() {
-  if (!linksGet || linksGet == '') {
-    console.log('LocalStorage was Empty')
-    localStorage.setItem('links', JSON.stringify(
-      [
-        "https://github.com/public-apis/public-apis",
-        "https://unicode.org/emoji/charts/full-emoji-list.html",
-        "https://www.w3schools.com/"
-      ]
-    ))
-  }
-  if (localStorage.getItem('snippets') == null || localStorage.getItem('snippets') == '') {
-    localStorage.setItem('snippets', {
-      snippets: [
-        'This is a snippet Title saved in localStorage:',
-        'This is a snippet. Saved to localStorage whenever you click save'
-      ]
-    })
+    if (!linksGet || linksGet == '') {
+        console.log('LocalStorage was Empty')
+        localStorage.setItem('links', JSON.stringify(
+            [
+                "https://github.com/public-apis/public-apis",
+                "https://unicode.org/emoji/charts/full-emoji-list.html",
+                "https://www.w3schools.com/"
+            ]
+        ))
+    }
+    if (localStorage.getItem('snippets') == null || localStorage.getItem('snippets') == '') {
+        localStorage.setItem('snippets', {
+            snippets: [
+                'This is a snippet Title saved in localStorage:',
+                'This is a snippet. Saved to localStorage whenever you click save'
+            ]
+        })
 
-  };
+    };
 }
 /*************************************************************************\
  **** Appending the DOM with info from Firestore on login******************\
@@ -39,63 +49,63 @@ function placeholderItems() {
 
 //Toggle UI based on signed in state
 function setupUI(user) {
-  console.log('setupUI Fired');
-  if (user) {
-    //toggle UI elements
-    $('.logged-in').show();
-    $('.logged-out').hide();
-    $('#greeting').show();
-    $('#emailDisplay').text(user.email);
-    //account info
-    fireStore.collection('users').doc(user.uid).get().then(function(doc) {
-      const userInfo = `
+    console.log('setupUI Fired');
+    if (user) {
+        //toggle UI elements
+        $('.logged-in').show();
+        $('.logged-out').hide();
+        $('#greeting').show();
+        $('#emailDisplay').text(user.email);
+        //account info
+        fireStore.collection('users').doc(user.uid).get().then(function(doc) {
+            const userInfo = `
           <div id="acctInfoEl">
           <p><span>Username:</span>${user.displayName}</p>
           <p><span>Email:</span>${user.email}</p>
           </div>`;
-      accountDetails.append(userInfo);
-    });
-  }
-  setupData(user);
+            accountDetails.append(userInfo);
+        });
+    }
+    setupData(user);
 }
 
 var snipObj = {
-  snippets: []
+    snippets: []
 }
 
 function setupData(user) {
-  console.log('setupData has fired');
-  console.log(user.uid);
-  let userDocRef = fireStore.collection('users').doc(user.uid)
-  //Go retireve the user data from Firestore
-  userDocRef.get('snippets').then((doc) => {
-    console.log(doc.data().snippets)
-    //If there is user data proceeed
-    if (doc.exists && doc.data() != null) {
-      for (var i = 0; i < doc.data().snippets.snippets.length; i++) {
-        snipObj.snippets.push(doc.data().snippets.snippets[i])
-      }
-      console.log(snipObj)
-      let setUpSnips = JSON.stringify(snipObj)
-      console.log('setUpSnips=', setUpSnips)
-      localStorage.setItem('snippets', setUpSnips)
+    console.log('setupData has fired');
+    console.log(user.uid);
+    let userDocRef = fireStore.collection('users').doc(user.uid)
+        //Go retireve the user data from Firestore
+    userDocRef.get('snippets').then((doc) => {
+        console.log(doc.data().snippets)
+            //If there is user data proceeed
+        if (doc.exists && doc.data() != null) {
+            for (var i = 0; i < doc.data().snippets.snippets.length; i++) {
+                snipObj.snippets.push(doc.data().snippets.snippets[i])
+            }
+            console.log(snipObj)
+            let setUpSnips = JSON.stringify(snipObj)
+            console.log('setUpSnips=', setUpSnips)
+            localStorage.setItem('snippets', setUpSnips)
 
-      // var setUpLinks = {
-      //   links: JSON.stringify([doc.data().links])
-      // }
-      //
-      // localStorage.setItem('links', JSON.parse(setUpLinks))
+            // var setUpLinks = {
+            //   links: JSON.stringify([doc.data().links])
+            // }
+            //
+            // localStorage.setItem('links', JSON.parse(setUpLinks))
 
-      appendSnips();
-      // appendLinks(setUpLinks);
-      //If not, don't.
-    } else {
-      console.log('No such Document');
-    }
-    /*After the above function has fired, invoke next function.
-    This prevents items from coming back as null or undefined
-    if it takes longer than expected to get the data back.*/
-  })
+            appendSnips();
+            // appendLinks(setUpLinks);
+            //If not, don't.
+        } else {
+            console.log('No such Document');
+        }
+        /*After the above function has fired, invoke next function.
+        This prevents items from coming back as null or undefined
+        if it takes longer than expected to get the data back.*/
+    })
 }
 // .then(function() {
 //   restoreData()
@@ -119,71 +129,71 @@ console.log(dayjs(now).format('dddd'))
 //LinksClock
 
 var renderClock = function() {
-  $("#currentTime").text(dayjs().format("h:mm a"));
-  var now = dayjs().format("dddd, MMM / D / YYYY");
-  $("#currentDay").text(now)
+    $("#currentTime").text(dayjs().format("h:mm a"));
+    var now = dayjs().format("dddd, MMM / D / YYYY");
+    $("#currentDay").text(now)
 }
 
 //Render clock every second
 $(function() {
-  setInterval(renderClock, 1000);
+    setInterval(renderClock, 1000);
 });
 //Nav navTabs
 $('#navDash').on('click', function() {
-  $('#workspace').children().hide();
-  $('#dash').show();
+    $('#workspace').children().hide();
+    $('#dash').show();
 
 })
 
 $('#navGit').on('click', function() {
-  $('#workspace').children().hide();
-  $('#git').show();
+    $('#workspace').children().hide();
+    $('#git').show();
 
 })
 
 $('#navCdnjs').on('click', function() {
-  $('#workspace').children().hide();
-  $('#cdnjs').show();
+    $('#workspace').children().hide();
+    $('#cdnjs').show();
 })
 
 $('#navCal').on('click', function() {
-  $('#workspace').children().hide();
-  $('#calendar').show();
+    $('#workspace').children().hide();
+    $('#calendar').show();
 })
 
 //close modal if popped
 $('#modal-cdnjs--close').on('click', function() {
-  $('#cdnjsInput').val('');
-  $('#modal-cdnjs').modal('close');
+    $('#cdnjsInput').val('');
+    $('#modal-cdnjs').modal('close');
 });
 
 // Week Planner
 
 function getEventText() {
-  var eventText = $(this).next(".inputField").val();
-  console.log($(this).next(".inputField").attr("id"))
-  var eventID = $(this).next(".inputField").attr("id");
-  localStorage.setItem(eventID, eventText);
+    var eventText = $(this).next(".inputField").val();
+    console.log($(this).next(".inputField").attr("id"))
+    var eventID = $(this).next(".inputField").attr("id");
+    localStorage.setItem(eventID, eventText);
 }
 $('.saveBtn').on('click', getEventText);
 $(function() {
-  var dayList = $(".inputField");
-  // console.log(dayList.length);
-  for (var i = 0; i < dayList.length; i++) {
-    // console.log(dayList[i]);
-    var dayStorage = dayList[i].id;
-    var setEventText = localStorage.getItem(dayStorage);
-    // console.log(setEventText);
-    $(`#${dayStorage}`).val(setEventText);
-  };
+    var dayList = $(".inputField");
+    // console.log(dayList.length);
+    for (var i = 0; i < dayList.length; i++) {
+        // console.log(dayList[i]);
+        var dayStorage = dayList[i].id;
+        var setEventText = localStorage.getItem(dayStorage);
+        // console.log(setEventText);
+        $(`#${dayStorage}`).val(setEventText);
+    };
 })
 $("#clearBtn").on("click", function() {
-  var dayList = $(".inputField");
-  console.log(dayList);
-  for (var i = 0; i < dayList.length; i++) {
-    console.log(dayList[i]);
-    console.log(`${dayList[i].id}`);
-    $(`#${dayList[i].id}`).val("");
-    localStorage.removeItem(`${dayList[i].id}`)
-  }
+    var dayList = $(".inputField");
+    console.log(dayList);
+    for (var i = 0; i < dayList.length; i++) {
+        console.log(dayList[i]);
+        console.log(`${dayList[i].id}`);
+        $(`#${dayList[i].id}`).val("");
+        localStorage.removeItem(`${dayList[i].id}`)
+    }
 });
--- assets/js/snippets.js
+++ assets/js/snippets.js
@@ -1,32 +1,32 @@
 $(function() {
-  //process contents of snippets
-  $("pre.snippet").each(function() {
+    //process contents of snippets
+    $("pre.snippet").each(function() {
 
-    //get contents of <pre> and create list and container
-    var currentItem = $(this);
-    var contents = (currentItem.text().indexOf("\n") != -1) ? currentItem.text().split("\n") : currentItem.text().split("\r")
-    var list = $("<ul>");
-    var container = $("<div>").addClass("snippet-container").insertBefore(currentItem.prev());
+        //get contents of <pre> and create list and container
+        var currentItem = $(this);
+        var contents = (currentItem.text().indexOf("\n") != -1) ? currentItem.text().split("\n") : currentItem.text().split("\r")
+        var list = $("<ul>");
+        var container = $("<div>").addClass("snippet-container").insertBefore(currentItem.prev());
 
-    //remove original contents and wrap in container
-    $(this).text("").prev().andSelf().appendTo(container);
+        //remove original contents and wrap in container
+        $(this).text("").prev().andSelf().appendTo(container);
 
-    //normalize height
-    (currentItem.height() > 0) ? list.css("marginTop", -16): null;
+        //normalize height
+        (currentItem.height() > 0) ? list.css("marginTop", -16): null;
 
-    //create list item
-    $.each(contents, function(i, val) {
+        //create list item
+        $.each(contents, function(i, val) {
 
-      //create list item and inner p
-      var li = $("<li>").appendTo(list),
-        p = $("<p>").text(val).appendTo(li);
+            //create list item and inner p
+            var li = $("<li>").appendTo(list),
+                p = $("<p>").text(val).appendTo(li);
 
-      //add stripe class
-      (i % 2 === 0) ? li.addClass("stripe"): li.addClass("unstripe");
+            //add stripe class
+            (i % 2 === 0) ? li.addClass("stripe"): li.addClass("unstripe");
+        });
+
+        list.appendTo(currentItem);
     });
-
-    list.appendTo(currentItem);
-  });
 });
 
 var snippetsFormEl = $('#snippets-form');
@@ -37,21 +37,22 @@ var snipsRemoverA = $("#snipsRemoverA");
 var snipsRemoverB = $("#snipsRemoverB");
 var enterBtn = $("#enter-btn");
 
-snipsRemoverA.on("click", theSnipsRemover)
-snipsRemoverB.on("click", theSnipsRemover)
+// snipsRemoverA.on("click", theSnipsRemover)
+// snipsRemoverB.on("click", theSnipsRemover)
 
 enterBtn.on('click', function() {
-  var localSnips = JSON.parse(localStorage.getItem('snippets'))
-  console.log(localSnips)
-  if (snipTitle.val() == '' || snipInput.val() == '') {
-    console.log(snipTitle.value)
-    console.log(snipInput.value)
-    var errMessage = "We couldn't save that one.";
-    $('#modal-error').modal('open');
-    $('#errmsg').text(errMessage);
-    $('#posSolution').text('Try typing something.');
-  } else {
-    var codeResult = $(`<div class="snipsDiv"> <pre class="snippet"><strong>${snipTitle.val().replace(/[<>&\n]/g, function(x) {
+    var localSnips = JSON.parse(localStorage.getItem('snippets'))
+    console.log(localSnips)
+    if (snipTitle.val() == '' || snipInput.val() == '') {
+        console.log(snipTitle.value)
+        console.log(snipInput.value)
+        var errMessage = "We couldn't save that one.";
+        $('#modal-error').modal('open');
+        $('#errmsg').text(errMessage);
+        $('#posSolution').text('Try typing something.');
+    } else {
+        var removeBtn = $('<button type=button class="rmSnips btn blue darken-2">Remove Snippy</button>')
+        var codeResult = $(`<div class="snipsDiv"> <pre class="snippet"><strong>${snipTitle.val().replace(/[<>&\n]/g, function(x) {
             return {
                 '<': '&lt;',
                 '>': '&gt;',
@@ -66,102 +67,132 @@ enterBtn.on('click', function() {
                '\n': '<br />'
             }[x];
         })}</pre></div>`);
-    snippetsListEl.append(codeResult);
-    var preppedTitle = codeResult[0].children[0].childNodes[0].childNodes[0].nodeValue;
-    var preppedCode = codeResult[0].children[0].childNodes[2].nodeValue;
-    console.log(preppedTitle);
-    console.log(preppedCode);
-    localSnips.snippets.push(preppedTitle, preppedCode);
-    console.log(localSnips)
-    let setUpSnips = JSON.stringify(localSnips);
-    localStorage.setItem('snippets', setUpSnips);
-  }
-  //Clear Input Fields
-  snipTitle.val("");
-  snipInput.val("");
-
-  sendToFS();
+        snippetsListEl.append(codeResult);
+        codeResult.append(removeBtn);
+        removeBtn.on('click', rmSnip)
+        var preppedTitle = codeResult[0].children[0].childNodes[0].childNodes[0].nodeValue;
+        var preppedCode = codeResult[0].children[0].childNodes[2].nodeValue;
+        console.log(preppedTitle);
+        console.log(preppedCode);
+        localSnips.snippets.push(preppedTitle, preppedCode);
+        console.log(localSnips)
+        let setUpSnips = JSON.stringify(localSnips);
+        localStorage.setItem('snippets', setUpSnips);
+    }
+    //Clear Input Fields
+    snipTitle.val("");
+    snipInput.val("");
+    packSnips();
 })
-var packedSnips = {
-  snippets: []
-}
-let lsSnippets = JSON.parse(localStorage.getItem('snippets'))
-let setUpSnips = lsSnippets.snippets
-for (let i = 0; i < lsSnippets.snippets.length; i++) {
 
-  packedSnips.snippets.push(setUpSnips[i])
+function packSnips() {
+    var packedSnips = {
+        snippets: []
+    }
+    let lsSnippets = JSON.parse(localStorage.getItem('snippets'))
+    console.log(lsSnippets)
+    let setUpSnips = lsSnippets.snippets
+    for (let i = 0; i < lsSnippets.snippets.length; i++) {
+        packedSnips.snippets.push(setUpSnips[i])
+    }
+    sendToFS(packedSnips)
 }
 
-function sendToFS() {
-  var userID = auth.currentUser.uid;
-  console.log(packedSnips)
-  console.log(userID)
-  let uiddocRef = fireStore.collection('users').doc(userID);
-  uiddocRef.set({
-    'snippets': packedSnips
-  }, {
-    merge: true
-  });
-  uiddocRef.get('snippets').then((doc) => {
-    console.log(doc.data())
-  });
+function rmSnip() {
+
+    let snipObj = JSON.parse(localStorage.getItem('snippets'));
+    console.log(snipObj)
+    let rmS = $(this).prev().children().last()[0].innerText;
+    let rmT = $(this).prev().children().first()[0].innerText;
+    console.log(snipObj.snippets)
+    let index = snipObj.snippets.indexOf(rmT);
+    console.log(index)
+    modSnips = snipObj.snippets.splice(index, 2)
+    console.log('modSnips=', modSnips)
+    console.log('snipObj=', snipObj)
+    localStorage.setItem('snippets', JSON.stringify(snipObj))
+    $(this).prev().remove();
+    $(this).remove();
+    packSnips()
+}
+
+function sendToFS(packedSnips) {
+    var userID = auth.currentUser.uid;
+    console.log(packedSnips)
+    console.log(userID)
+    let uiddocRef = fireStore.collection('users').doc(userID);
+    uiddocRef.set({
+        'snippets': packedSnips
+    }, {
+        merge: true
+    });
+    uiddocRef.get('snippets').then((doc) => {
+        console.log(doc.data())
+    });
 }
 
 function appendSnips() {
-  console.log('appendSnips has fired')
-  let setUpSnips = JSON.parse(localStorage.getItem('snippets'))
+    console.log('appendSnips has fired')
+    let setUpSnips = JSON.parse(localStorage.getItem('snippets'))
 
-  if (setUpSnips.snippets != null && setUpSnips.snippets != '') {
-    for (var i = 0, n = 1; i < setUpSnips.snippets.length; i += 2, n += 2) {
-      // if (i % 2 === 0 && i !== 0) {
-      //   // Do the even number one, concatenating to the previous
-      //   var snipsTitle = setUpSnips.snippets[i]
-      // } else {
-      //   //  Do the odd number one
-      //   var snipsText = setUpSnips.snippets[i]
-      // }
-      var codeResultForLoop = $('<div class="snipsDiv"> <pre class="snippet"><p><strong>' + setUpSnips.snippets[i] + '</strong></p><br/><p>' + setUpSnips.snippets[n] + '</p></pre></div>');
-      snippetsListEl.append(codeResultForLoop);
+    if (setUpSnips.snippets != null && setUpSnips.snippets != '') {
+        for (var i = 0, n = 1; i < setUpSnips.snippets.length; i += 2, n += 2) {
+            var removeBtn = $('<button type=button class="rmSnips btn blue darken-1">Remove Snippy</button>')
+                // if (i % 2 === 0 && i !== 0) {
+                //   // Do the even number one, concatenating to the previous
+                //   var snipsTitle = setUpSnips.snippets[i]
+                // } else {
+                //   //  Do the odd number one
+                //   var snipsText = setUpSnips.snippets[i]
+                // }
+            var codeResultForLoop = $('<div class="snipsDiv"> <pre class="snippet"><p><strong>' + setUpSnips.snippets[i] + '</strong></p><br/><p>' + setUpSnips.snippets[n] + '</p></pre></div>');
+            snippetsListEl.append(codeResultForLoop);
+            codeResultForLoop.append(removeBtn);
+            removeBtn.on('click', rmSnip)
+
+        }
+    } else {
+        placeholderItems();
+        console.log('setUpSnips was null')
     }
-  } else {
-    placeholderItems();
-    console.log('setUpSnips was null')
-  }
 
 };
 
-function theSnipsRemover() {
-  var theSnips = $(".snipsDiv");
-  if (typeof JSON.parse(localStorage.getItem("ls-snippets")) === "object") {
-    var lsSnips = JSON.parse(localStorage.getItem("ls-snippets"));
-    var lsSnipsSnippets = lsSnips.snippets;
-    if ($(this).attr("class") == "fullRemover") {
-      // Remove Loop
-      for (var i = 0; i < theSnips.length; i++) {
-        theSnips[i].remove();
-        lsSnipsSnippets.pop();
-        localStorage.setItem("ls-snippets", JSON.stringify(lsSnipsSnippets));
-      }
-    } else if ($(this).attr("class") == "lastRemover") {
-      var i = theSnips.length - 1;
-      console.log(theSnips);
-      theSnips[i].remove();
-      lsSnipsSnippets.pop();
-      localStorage.setItem("ls-snippets", JSON.stringify(lsSnipsSnippets));
-    }
-  } else {
-    if ($(this).attr("class") == "fullRemover") {
-      // Remove Loop
-      for (var i = 0; i < theSnips.length; i++) {
-        theSnips[i].remove();
-        lsSnips = [];
-        localStorage.setItem("ls-snippets", lsSnips);
-      }
-    } else if ($(this).attr("class") == "lastRemover") {
-      var i = theSnips.length - 1;
-      theSnips[i].remove();
-      lsSnipsSnippets = [];
-      localStorage.setItem("ls-snippets", lsSnipsSnippets);
-    }
-  }
-}
+
+
+// function theSnipsRemover() {
+//     var theSnips = $(".snipsDiv");
+//     if (typeof JSON.parse(localStorage.getItem("ls-snippets")) === "object") {
+
+//         var lsSnips = JSON.parse(localStorage.getItem("ls-snippets"));
+//         console.log(setUpSnips);
+//         if ($(this).attr("class") == "fullRemover") {
+//             // Remove Loop
+//             for (var i = 0; i < theSnips.length; i++) {
+//                 theSnips[i].remove();
+//                 lsSnipsSnippets.pop();
+//                 localStorage.setItem("ls-snippets", JSON.stringify(lsSnipsSnippets));
+//             }
+//         } else if ($(this).attr("class") == "lastRemover") {
+//             var i = theSnips.length - 1;
+//             console.log(theSnips);
+//             theSnips[i].remove();
+//             lsSnipsSnippets.pop();
+//             localStorage.setItem("ls-snippets", JSON.stringify(lsSnipsSnippets));
+//         }
+//     } else {
+//         if ($(this).attr("class") == "fullRemover") {
+//             // Remove Loop
+//             for (var i = 0; i < theSnips.length; i++) {
+//                 theSnips[i].remove();
+//                 lsSnips = [];
+//                 localStorage.setItem("ls-snippets", lsSnips);
+//             }
+//         } else if ($(this).attr("class") == "lastRemover") {
+//             var i = theSnips.length - 1;
+//             theSnips[i].remove();
+//             lsSnipsSnippets = [];
+//             localStorage.setItem("ls-snippets", lsSnipsSnippets);
+//         }
+//     }
+// }
